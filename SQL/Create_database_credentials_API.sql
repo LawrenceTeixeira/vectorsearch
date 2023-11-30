2dc872e873a2ccb99a3d445823e0ea14a5a35eb5
LawrenceTeixeira <43327665+LawrenceTeixeira@users.noreply.github.com>
2023-11-30 14:15:17 -0300

    new file:   .gitignore 	modified:   SQL/Create_database_credentials_API.sql

--- SQL/Create_database_credentials_API.sql
+++ SQL/Create_database_credentials_API.sql
@@ -1,3 +1,8 @@
+if not exists(select * from sys.symmetric_keys where [name] = '##MS_DatabaseMasterKey##')
+begin
+	create master key encryption by password = 'Pa$$w0rd!'
+end
+
 /*
     Create database credentials to store API key
 */
